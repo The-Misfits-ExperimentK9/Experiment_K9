1fda5a2df16a36240b5b77a290198f9b2ea0ff9c
meaton96 <lammara96@gmail.com>
2023-11-21 20:50:43 -0500

    Changed 2d receiver to 3d object, 2d dropped sphere is still a sphere
    these changes fix the shaders causing 2d objects to disappear
    Also a bunch of lightning data (just base environmental lightning)

--- Assets/Scripts/objects/TransferableObject.cs
+++ Assets/Scripts/objects/TransferableObject.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TransferableObject : GrabbableObject {
-    
+
     [SerializeField] private GameObject displayObject_2D;
     [SerializeField] private SpriteRenderer spriteRenderer2D;
     [SerializeField] private float objectDrawOffset = 4f;
@@ -18,21 +18,21 @@ public class TransferableObject : GrabbableObject {
         sphere = displayObject3D_Mesh.GetComponent<SphereCollider>();
         //turn off the physics if the object starts as 2D
         //interactDisplayController.SetInteractIndicatorActive(true);
-        if (!Is3D) { 
-            
+        if (!Is3D) {
+
             Disable3D();
         }
     }
     private void Update() {
         if (!Is3D) {
             //if (displayObject_2D.transform.localPosition != Vector3.zero) {
-                //reset position of sphere and this objec to the location of the 2d sprite while in 2d mode
-                //reset position of the 3d object to the 2d object location
-                transform.position = displayObject_2D.transform.position;
-                displayObject3D_Mesh.transform.position= displayObject_2D.transform.position;
+            //reset position of sphere and this objec to the location of the 2d sprite while in 2d mode
+            //reset position of the 3d object to the 2d object location
+            transform.position = displayObject_2D.transform.position;
+            displayObject3D_Mesh.transform.position = displayObject_2D.transform.position;
             displayObject_2D.transform.localPosition = Vector3.zero;
-                
-           // }
+
+            // }
         }
     }
 
@@ -47,16 +47,16 @@ public class TransferableObject : GrabbableObject {
         displayObject3D_Mesh.enabled = true;
     }
     public void Enable2D() {
-        
+
         displayObject_2D.SetActive(true);
-       
+
     }
     public void Disable3D() {
         //TogglePhysics(disable: true);
         //turn off colliders so it doesn't drag theplayer
-        
+
         sphere.enabled = false;
-        sphere.excludeLayers = LayerMask.GetMask("Player");
+        // sphere.excludeLayers = LayerMask.GetMask("Player");
         displayObject3D_Mesh.enabled = false;
         Is3D = false;
     }
@@ -73,20 +73,20 @@ public class TransferableObject : GrabbableObject {
 
     public void SetHoldArea(GameObject holder, Transform holdArea) {
         Debug.Log("setting hold: " + holder.name);
-        
-        this.holder = holder;   
+
+        this.holder = holder;
         transform.parent = holdArea;
         transform.position = holdArea.position;
         //transform.localPosition = offset;
     }
     public void Pickup2D(GameObject holder) {
-        
 
+        displayObject3D_Mesh.enabled = false;
         //set the hold as the parent to carry it around
         transform.SetParent(holder.transform);
         spriteRenderer2D.enabled = false;
         sphere.enabled = false;
-        sphere.excludeLayers = LayerMask.GetMask("Player");
+        //  sphere.excludeLayers = LayerMask.GetMask("Player");
         //disable interaction indicator
         interactDisplayController.SetInteractIndicatorActive(false);
         this.holder = holder;
@@ -97,35 +97,36 @@ public class TransferableObject : GrabbableObject {
     }
     public override void DropObject() {
         Debug.Log("transferable object drop object");
-        if (Is3D) 
+        if (Is3D)
             Drop3D();
         else
             Drop2D();
     }
-    
 
-    
+
+
     public void Drop2D() {
         Debug.Log("Dropping 2d object");
-        
+
         transform.forward = holder.transform.forward;
 
         //calculate an offset based on the players right axis
         bool facingRight = holder.GetComponent<MovementController_2D>().IsFlipped();
         var offset = (facingRight ? objectDrawOffset : -objectDrawOffset) * holder.transform.right;
-  
-        displayObject_2D.SetActive(true);   
-        spriteRenderer2D.enabled = true;
+
+        //  displayObject_2D.SetActive(true);   
+        //  spriteRenderer2D.enabled = true;
+
         sphere.enabled = true;
-        sphere.excludeLayers = LayerMask.GetMask("");
+        sphere.excludeLayers = LayerMask.GetMask("Walls");
 
         holder = null;
         IsBeingHeld = false;
         transform.parent = null;
         transform.position += offset;
-
+        displayObject3D_Mesh.enabled = true;
         interactDisplayController.SetInteractIndicatorActive(true);
         interactDisplayController.ResetPosition();
     }
-    
+
 }
