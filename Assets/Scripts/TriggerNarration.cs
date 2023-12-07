afe76c2b28c78d25cfd2c15274c9eaa10baa3b1a
meaton96 <lammara96@gmail.com>
2023-12-07 15:45:23 -0500

    lighting + sound trigger

---
+++ Assets/Scripts/TriggerNarration.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerNarration : MonoBehaviour
+{
+    public AudioClip narrationClip; 
+    private AudioSource audioSource;
+    [SerializeField] private bool onlyPlayOnce = true;
+
+    void Start() {
+        if (!narrationClip) {
+            Debug.LogError("No narration clip assigned to " + gameObject.name);
+        }
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.clip = narrationClip;
+        audioSource.spatialBlend = 0; 
+        audioSource.playOnAwake = false;
+    }
+    private void Update() {
+        if (onlyPlayOnce) {
+            if (!audioSource.isPlaying && audioSource.time > 0) {
+                gameObject.SetActive(false); 
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == LayerInfo.PLAYER) {
+            PlayNarration();
+        }
+    }
+
+    private void PlayNarration() {
+        if (!audioSource.isPlaying) {
+            audioSource.Play();
+        }
+    }
+}
