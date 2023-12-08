149f7406a7ffe980d4073d4a9e04f23ec8b05e46
meaton96 <lammara96@gmail.com>
2023-12-07 19:35:11 -0500

    sound trigger prefab + main menu scaling

---
+++ Assets/Scripts/TriggerNarration.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerNarration : MonoBehaviour
+{
+    public AudioClip narrationClip; 
+    private AudioSource audioSource;
+    [SerializeField] private bool onlyPlayOnce = true;
+    [SerializeField] private ActivatablePuzzlePiece puzzlePieceToActivate;
+
+    void Start() {
+        if (!narrationClip) {
+            Debug.LogError("No narration clip assigned to " + gameObject.name);
+        }
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.clip = narrationClip;
+        audioSource.spatialBlend = 0; 
+        audioSource.playOnAwake = false;
+    }
+    private void Update() {
+        if (onlyPlayOnce) {
+            if (!audioSource.isPlaying && audioSource.time > 0) {
+                if (puzzlePieceToActivate)
+                    puzzlePieceToActivate.Activate();
+                gameObject.SetActive(false); 
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == LayerInfo.PLAYER) {
+            PlayNarration();
+        }
+    }
+
+    private void PlayNarration() {
+        if (!audioSource.isPlaying) {
+            audioSource.Play();
+        }
+    }
+}
